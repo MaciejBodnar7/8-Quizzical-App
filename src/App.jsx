9a5b674953f3d120573f3107075b90620202051c
MaciejBodnar7 <maciej.bodnar2@gmail.com>
2024-06-02 11:40:01 +0200

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./App.css"
+import StartApp from "./components/StartApp"
+import MainApp from "./components/MainApp"
+
+function App() {
+  return (
+    <>
+      <StartApp />
+      <MainApp />
+    </>
+  )
+}
+
+export default App
